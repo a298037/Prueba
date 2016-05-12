e86956ddff7a1a63b5cef7e158e279609a6aa774
a298037 <a298037@uach.mx>
2016-05-12 11:34:28 -0600

    Update Prueba.java

--- prueba/src/prueba/Prueba.java
+++ prueba/src/prueba/Prueba.java
@@ -18,6 +18,7 @@ public class Prueba {
      */
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Prueba");
+         JOptionPane.showMessageDialog(null, "Hola");
         // TODO code application logic here
     }
     
